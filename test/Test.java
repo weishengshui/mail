6b47767a2959e75c3a35ccc1e999f13eaf3b9128
weishengshui <657620636@qq.com>
2012-10-29 14:04:00 +0800

    xxx

--- test/Test.java
+++ test/Test.java
@@ -8,8 +8,7 @@ public class Test {
 	@org.junit.Test
 	public void test() {
 		SendEmailBean sendEmailBean = new SendEmailBean();
-		sendEmailBean.sendSimpleEmail("914463145@qq.com", "你好，水哥！", "测试邮箱程序");
-		sendEmailBean.sendHtmlEmail("1349317563@qq.com", "你好，华妹子！\n这是我用程序发给你的", "测试邮箱程序");
+		sendEmailBean.sendSimpleEmail("657620636@qq.com", "你好，水哥！", "测试邮箱程序");
 		
 	}
 
